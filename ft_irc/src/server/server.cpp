ac590664d918fe7443693100c7fc46e079786fdb
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-01-16 17:46:08 +0100

    add system functions, server loop, init

--- ft_irc/src/server/server.cpp
+++ ft_irc/src/server/server.cpp
@@ -5,13 +5,39 @@
 Server::Server() { _servSocketFd = -1; }
 
 void Server::servInit(int port, char *password) {
-	this->_port = port;
-	this->_password = std::string(password);
-	servSocket();
+  this->_port = port;
+  this->_password = std::string(password);
+  servSocket();
+  this->_signal = 1;
+  servLoop();
 
-	std::cout << "Server #" << _servSocketFd << " connected" << 
-	std::endl;
-	std::cout << "Waiting for clients\n";
+  std::cout << "Server #" << _servSocketFd << " connected" << std::endl;
+  std::cout << "Waiting for clients\n";
+}
+
+void Server::servLoop() {
+  while (this->_signal) {
+    int ready = poll(&_fds[0], _fds.size(), -1);
+
+    if (ready == -1)
+      throw(std::runtime_error("poll failed"));
+
+    for (size_t i = 0; i < _fds.size() && ready > 0; i++) {
+      if (_fds[i].revents == 0)
+        continue;
+
+      --ready;
+
+      if (_fds[i].revents & POLLIN)
+        handleRead(_fds[i].fd);
+
+      if (_fds[i].revents & POLLOUT)
+        handleWrite(_fds[i].fd);
+
+      if (_fds[i].revents & (POLLERR | POLLHUP | POLLNVAL))
+        disconnectClient(_fds[i].fd);
+    }
+  }
 }
 
 void Server::servSocket() {
@@ -23,29 +49,61 @@ void Server::servSocket() {
 
   _servSocketFd = socket(AF_INET, SOCK_STREAM, 0);
   if (_servSocketFd == -1)
-    throw(std::runtime_error("faild to create socket"));
+    throw(std::runtime_error("failed to create socket"));
+
+  int reuse = 1;
+  if (setsockopt(_servSocketFd, SOL_SOCKET, SO_REUSEADDR, &reuse,
+                 sizeof(reuse)) == -1) {
+    close(_servSocketFd);
+    throw(std::runtime_error("failed to set SO_REUSEADDR"));
+  }
+
+  if (fcntl(_servSocketFd, F_SETFL, O_NONBLOCK) == -1) {
+    close(_servSocketFd);
+    throw(std::runtime_error("failed to set O_NONBLOCK"));
+  }
+
+  if (bind(_servSocketFd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    close(_servSocketFd);
+    throw(std::runtime_error("failed to bind socket"));
+  }
+
+  if (listen(_servSocketFd, SOMAXCONN) == -1) {
+    close(_servSocketFd);
+    throw(std::runtime_error("failed to listen"));
+  }
 
   _newPoll.fd = _servSocketFd;
   _newPoll.events = POLLIN;
   _newPoll.revents = 0;
   _fds.push_back(_newPoll);
-
-  /* add socket options, bind?, listen */
 }
 
-/* Erasing at interator downshifts the vector, so to avoid skipping over
-   element reverse interation is used
-*/
+void Server::handleRead(int fd) {
+  char buffer[512];
+  ssize_t n = recv(fd, buffer, sizeof(buffer), 0);
 
-// TODO -> the clean exit logic need to be coded in both Destructor and signalHandler to make sure that the program exit always properly
-void Server::signalHandler(int signum)
-{
-	if (signum == SIGINT || signum == SIGQUIT)
-		exit(0);
+  if (n <= 0) {
+    disconnectClient(fd);
+    return;
+  }
+
+  clientBuffer[fd].append(buffer, n);
+  std::cout << clientBuffer[fd] << std::endl;
+}
+
+// TODO -> the clean exit logic need to be coded in both Destructor and
+// signalHandler to make sure that the program exit always properly
+void Server::signalHandler(int signum) {
+  if (signum == SIGINT || signum == SIGQUIT)
+    exit(0);
 }
 
 Server::~Server() {}
 
+/* Erasing at interator downshifts the vector, so to avoid skipping over
+   element reverse interation is used
+*/
 void Server::clearClients(int fd) {
   for (int i = _fds.size() - 1; i >= 0; i--) {
     if (_fds[i].fd == fd) {
